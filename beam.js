c8481115535a8d3b246cced7cebcb860db325044
momoney13 <64868507+momoney13@users.noreply.github.com>
2021-07-14 12:51:26 -0400

    Finished videos 10, 11, and 12
    Added a score system, feedback, and audio to my game. It is now runnable and this is the final commit!

--- beam.js
+++ beam.js
@@ -1,5 +1,6 @@
 class Beam extends Phaser.GameObjects.Sprite{
   constructor(scene){
+
     var x = scene.player.x;
     var y = scene.player.y - 16;
 
@@ -10,8 +11,9 @@ class Beam extends Phaser.GameObjects.Sprite{
     this.play("beam_anim");
     scene.physics.world.enableBody(this);
     this.body.velocity.y = - 250;
-    
+
     scene.projectiles.add(this);
+  
 
   }
 
--- game.js
+++ game.js
@@ -1,21 +1,23 @@
-var config = {
-    width: 256,
-    height: 272,
-    backgroundColor: 0x000000,
-    scene: [Scene1, Scene2],
-    pixelArt: true,
-
-    physics: {
-        default: "arcade",
-        arcade:{
-            debug: false
-        }
-    }
-
+var gameSettings = {
+  playerSpeed: 200,
+  maxPowerups: 2,
+  powerUpVel: 50,
 }
 
-    var game=new Phaser.Game(config);
-
-    var gameSettings = {
-        playerSpeed: 200,
+var config = {
+  width: 256,
+  height: 272,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2],
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade:{
+        debug: false,
+        debugShowVelocity: false
     }
+  }
+}
+
+
+var game = new Phaser.Game(config);
